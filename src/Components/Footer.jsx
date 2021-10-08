d128f16b34452e8de78bfb8728fcb1c59acd3842
Utkarsh Dhiman <utkarshdhiman48@gmail.com>
2021-10-08 18:43:34 +0530

    fix links

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import '../Assets/styles/Footer.css';
-import { Link } from 'react-router-dom';
 import { ReactComponent as Github } from '../Assets/images/github.svg';
 import { ReactComponent as Twitter } from '../Assets/images/twitter.svg';
 import { ReactComponent as Linkedin } from '../Assets/images/linkedin.svg';
@@ -9,18 +8,18 @@ import { ReactComponent as Mail } from '../Assets/images/mail.svg';
 export default function Footer(props) {
   return (<footer className="glass02 p-1r m-sapt" id="contact">
 		<div className="contact">
-			<Link className="contact-item" title="Github" to="https://github.com/utkarsh48">
+			<a className="contact-item" title="Github" target="_blank" rel="noreferrer" href="https://github.com/utkarsh48">
 				<Github />
-			</Link>
-			<Link className="contact-item" title="Twitter" to="https://twitter.com/utkarshdhiman48">
+			</a>
+			<a className="contact-item" title="Twitter" target="_blank" rel="noreferrer" href="https://twitter.com/utkarshdhiman48">
 				<Twitter />
-			</Link>
-			<Link className="contact-item" title="LinkedIn" to="https://www.linkedin.com/in/utkarsh-dhiman-b626b5149/">
+			</a>
+			<a className="contact-item" title="LinkedIn" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/utkarshdhiman48/">
 				<Linkedin />
-			</Link>
-			<Link className="contact-item" title="email" to="mailto:utkarshdhiman48@gmail.com">
+			</a>
+			<a className="contact-item" title="email" target="_blank" rel="noreferrer" href="mailto:utkarshdhiman48@gmail.com">
 				<Mail />
-			</Link>
+			</a>
 		</div>
     <hr className="max-w-half" />
     <div className="text-center m-halfr"><span>© Utkarsh Dhiman</span></div>
